ba94780b323ba86649898d9423020534bb838b9e
Clément Baudet <10958405+cbdt@users.noreply.github.com>
2025-04-26 13:54:09 +0200

    repro

---
+++ index.tsx
@@ -0,0 +1,13 @@
+// run `tsx index.ts` in the terminal
+
+import { render } from "jsx-email";
+import { Template } from "./template.js";
+import React from "react";
+
+console.log("hello");
+
+// await render(<Template />)
+
+throw new Error("test");
+
+console.log("hello2");
---
+++ template.tsx
@@ -0,0 +1,6 @@
+import {
+  Html,
+} from 'jsx-email';
+import React from 'react';
+
+export const Template = () => <Html></Html>
