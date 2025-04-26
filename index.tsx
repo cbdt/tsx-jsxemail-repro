191225d575fd536a2e30285780b0990570ed2f31
Clément Baudet <10958405+cbdt@users.noreply.github.com>
2025-04-26 13:57:15 +0200

    repro

--- index.tsx
+++ index.tsx
@@ -1,12 +1,12 @@
-// run `tsx index.ts` in the terminal
-
 import { render } from "jsx-email";
 import { Template } from "./template.js";
 import React from "react";
 
 console.log("hello");
 
-// await render(<Template />)
+// Comment this line and the error will be correctly mapped.
+// Any reference to jsx-email seems to cause source-map issues.
+await render(<Template />)
 
 throw new Error("test");
 
